a919891bc6ce2117b02bdbad56d5fe8105de4406
Adam Taylor <adam@appsorcery.com>
2016-04-20 21:27:36 -0700

    khghg

--- boot-scripts/install_node.sh
+++ boot-scripts/install_node.sh
@@ -3,13 +3,13 @@
 coreo_dir="$(pwd)"
 files_dir="$(pwd)/../files"
 
-#yum -y update
+yum -y update
 
 #install thumbor
 yum -y install python-devel gcc autoconf.noarch automake git
 yum -y install libjpeg-turbo-devel.x86_64 libjpeg-turbo-utils.x86_64 libtiff-devel.x86_64 libpng-devel.x86_64 pngcrush jasper-devel.x86_64 libwebp-devel.x86_64 python-pip 
 pip install pycurl  
-pip install numpy
+#pip install numpy
 
 git clone https://github.com/kohler/gifsicle.git  
 cd gifsicle  
