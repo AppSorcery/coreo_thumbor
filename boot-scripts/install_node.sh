d8a546bf79f9d6fd8170617c5f07e5de590710ff
Adam Taylor <adam@appsorcery.com>
2016-04-22 18:02:49 -0700

    ...

--- boot-scripts/install_node.sh
+++ boot-scripts/install_node.sh
@@ -6,10 +6,9 @@ files_dir="$(pwd)/../files"
 #yum -y update
 
 #install thumbor
-#yum -y install python-devel gcc autoconf.noarch automake git
-#yum -y install libjpeg-turbo-devel.x86_64 libjpeg-turbo-utils.x86_64 libtiff-devel.x86_64 libpng-devel.x86_64 pngcrush jasper-devel.x86_64 libwebp-devel.x86_64 python-pip 
-pip install pycurl tc_aws thumbor boto3
-#pip install numpy
+yum -y install python-devel gcc autoconf.noarch automake git
+yum -y install libjpeg-turbo-devel.x86_64 libjpeg-turbo-utils.x86_64 libtiff-devel.x86_64 libpng-devel.x86_64 pngcrush jasper-devel.x86_64 libwebp-devel.x86_64 python-pip 
+pip install pycurl numpy tc_aws thumbor boto3
 
 yum -y install  
 git clone https://github.com/kohler/gifsicle.git  
